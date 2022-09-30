554a1c893971e3a93a18e116975261b6796679df
simonjiaoh <simonjiaoh@gmail.com>
2022-09-30 11:48:27 +0800

    update 876

--- leetcode/src/ltcn75plan.rs
+++ leetcode/src/ltcn75plan.rs
@@ -134,29 +134,15 @@ impl Solution {
 
     // =876=
     pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        let mut tmp = &head;
-        let mut len: usize = 0;
-        while tmp.is_some() {
-            len += 1;
-            tmp = &tmp.as_ref().unwrap().next;
-        }
-        let odd = len % 2 == 1;
-        let mut node = if odd { len / 2 + 1 } else { len / 2 };
+        let mut fast = &head;
+        let mut slow = &head;
 
-        let mut tmp = head;
-        while tmp.is_some() {
-            node -= 1;
-            if node == 0 {
-                return if odd {
-                    tmp
-                } else {
-                    tmp.as_mut().unwrap().next.take()
-                };
-            }
-            tmp = tmp.as_mut().unwrap().next.take();
+        while fast.is_some() && fast.as_ref().unwrap().next.is_some() {
+            slow = &slow.as_ref().unwrap().next;
+            fast = &fast.as_ref().unwrap().next.as_ref().unwrap().next;
         }
-        // only for a empty linklist
-        tmp
+
+        slow.clone()
     }
 }
 
