14d54094cf87d29180c56cde99033dd5dfa6dbee
simonjiaoh <simonjiaoh@gmail.com>
2022-10-08 13:50:18 +0800

    589

--- leetcode/src/algo.c
+++ leetcode/src/algo.c
@@ -2,12 +2,12 @@
 // Created by simon on 2022/9/30.
 //
 // Definition for single-linked list
+#include<stdlib.h>
+
 struct ListNode {
     int val;
     struct ListNode *next;
 };
-
-#include<stdlib.h>
 
 // =142=
 struct ListNode *detectCycle(struct ListNode *head) {
@@ -26,4 +26,37 @@ struct ListNode *detectCycle(struct ListNode *head) {
         }
     }
     return NULL;
+}
+
+/**
+ * Definition for Node in problem 589
+ */
+struct Node {
+    int val;
+    int numChildren;
+    struct Node** children;
+};
+
+void helper(const struct Node* root, int* res, int* pos) {
+    if (root == NULL) {
+        return;
+    }
+
+    res[(*pos)++] = root->val;
+    for (int i=0; i<root->numChildren; i++) {
+        helper(root->children[i], res, pos);
+    }
+}
+
+int* preorder(struct Node* root, int* returnSize) {
+    //1. 减少内存分配，假设一个最大值，题目中已经给出 [0,10^4]
+    //2. 递归
+    //3. 必须使用栈记录现在遍历的位置，避免陷入思维误区，要注意套路
+#define MAX_NODE_SIZE 10000
+
+    int* res = (int*)malloc(MAX_NODE_SIZE * sizeof(int));
+    int pos = 0;
+    helper(root, res, &pos);
+    *returnSize = pos;
+    return res;
 }
